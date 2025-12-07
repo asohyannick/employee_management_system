e6c5594d05011393f6f006cd82c393366d2ab348
asohyannick <asoh@cskytelecoms.com>
2025-12-07 21:58:02 +0100

    resolve a bug after a failed deployment and try to redeploy again successfully

--- src/common/config/ConfigModule.ts
+++ src/common/config/ConfigModule.ts
@@ -11,12 +11,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
-                host: configService.get<string>('DATABASE_HOST'),
-                port: configService.get<number>('DATABASE_PORT'),
-                username: configService.get<string>('DATABASE_USERNAME'),
-                password: configService.get<string>('DATABASE_PASSWORD'),
-                database: configService.get<string>('DATABASE_NAME'),
-                synchronize: true,
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                synchronize: false,
                 autoLoadEntities: true,
                 ssl: {
                     rejectUnauthorized: false,
